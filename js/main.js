5b89b5b45cf22a9321d0926712ee1c2b73a36c86
AuroraAnima13 <168267701+AuroraAnima13@users.noreply.github.com>
2024-05-23 18:38:37 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,170 +1,46 @@
-let timerInterval
-let startTime
-
-function startTimer() {
-  startTime = Date.now()
-  timerInterval = setInterval(updateTimer, 100)
-}
-
-function updateTimer() {
-  const elapsedTime = Date.now() - startTime
-  const totalSeconds = Math.floor(elapsedTime / 1000)
-
-  const hours = Math.floor(totalSeconds / 3600)
-  const minutes = Math.floor((totalSeconds % 3600) / 60)
-  const seconds = totalSeconds % 60
-
-  const formattedTime =
-    padZero(hours) + ":" + padZero(minutes) + ":" + padZero(seconds)
-  const timerElement = document.getElementById("timer")
-  if (timerElement) {
-    timerElement.innerText = formattedTime
-  }
-}
-
-function padZero(num) {
-  return (num < 10 ? "0" : "") + num
-}
-async function initMapWithDelay() {
-  await ymaps3.ready
-
-  
-    const { YMap, YMapDefaultSchemeLayer } = ymaps3
-
-    const map = new YMap(document.getElementById("map"), {
-      location: {
-        center: [39.267048, 51.71401],
-        zoom: 16,
-      },
-    })
-
-    map.addChild(new YMapDefaultSchemeLayer())
-    document.getElementById("loader").style.display = "none"
-    document.getElementById("map").style.display = "block"
-  
-}
-function hideElement() {
-  const hideButton = document.getElementById("close-btn")
-  const elementToHide = document.getElementById("item")
-  if (!hideButton) {
-    return
-  }
-  hideButton.addEventListener("click", function () {
-    elementToHide.style.display = "none"
+$(function () {
+  $(".rightside-menu--close-btn").on("click", () => {
+    $(".rightside-menu").addClass("rightside-menu--close")
   })
-}
 
-function reloadMap() {
-  const map = document.getElementById("map")
-  const btn = document.getElementById("reloadMapBtn")
-  if (!map) {
-    return
-  }
-  btn.addEventListener("click", function () {
-    if (map.firstChild) {
-      map.removeChild(map.firstChild)
-    }
-    map.style.display = "none"
-    document.getElementById("loader").style.display = "block"
-    initMapWithDelay()
+  $(".menu__btn").on("click", () => {
+    $(".rightside-menu").removeClass("rightside-menu--close")
   })
-}
-
-function resetTimer() {
-  const btn = document.getElementById("btnn")
-  const timer = document.getElementById("timer")
-  if (!timer) {
-    return
-  }
-  btn.addEventListener("click", function () {
-    clearInterval(timerInterval)
-    timer.innerText = "00:00:00"
-    startTimer()
+  $(".top__slider").slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
   })
-}
-
-function doActiveLink() {
-  // активная ссылка
-  const items = document.querySelectorAll(".nav__item")
-  const links = document.querySelectorAll(".nav__link")
-
-  links.forEach(function (link) {
-    link.addEventListener("click", function (event) {
-      items.forEach(function (item) {
-        item.classList.remove("nav__item--active")
-      })
-
-      link.parentNode.classList.add("nav__item--active")
-    })
+  $(".contact__slider-inner").slick({
+    slidesToShow: 10,
+    slidesToScroll: 10,
+    dots: true,
+    arrows: false,
   })
-}
-
-function showHideRightMenu() {
-  const btn = document.getElementById("rightMenu__close--btn")
-  const menu = document.querySelector(".rightMenu")
-  const btn2 = document.querySelector(".burger__menu-btn")
-  btn.addEventListener("click", function (event) {
-    menu.classList.remove("rightMenu--close")
+  $(".article-slider-box ").slick({
+    prevArrow: `      <button
+        type="button"
+        class="article-slider__arrow article-slider__arrow-left"
+      >
+        <img src="images/prev.svg" alt="" />
+      </button>
+    `,
+    nextArrow: `      <button
+        type="button"
+        class="article-slider__arrow article-slider__arrow-right"
+      >
+        <img src="images/next.svg" alt="" />
+      </button>`,
   })
-  btn2.addEventListener("click", function (event) {
-    menu.classList.add("rightMenu--close")
+
+  let mixer = mixitup(".gallery__inner", {
+    load: {
+      filter: ".bedroom",
+    },
   })
-}
-
-function swup() {
-  const swupDiv = document.querySelector(".swup")
-  const navLinks = document.querySelectorAll(".nav__link")
-
-  // =================== Обработчики =================
-  const loadScripts = (href) => {
-    if (href.includes("map")) {
-      initMapWithDelay()
-      hideElement()
-      reloadMap()
-    }
-    if (href.includes("timer")) {
-      resetTimer()
-      hideElement()
-    }
-  }
-
-  const loadPage = (href) => {
-    fetch(href)
-      .then((response) => response.text())
-      .then((html) => {
-        const parser = new DOMParser()
-        const doc = parser.parseFromString(html, "text/html")
-        const newContent = doc.querySelector(".swup").innerHTML
-
-        swupDiv.innerHTML = newContent
-        document.title = doc.title
-
-        history.pushState({}, "", href)
-      })
-      .then(() => {
-        loadScripts(href)
-      })
-  }
-
-  navLinks.forEach((el) => {
-    el.addEventListener("click", (e) => {
-      e.preventDefault()
-
-      let href = e.currentTarget.getAttribute("href")
-      loadPage(href)
-    })
-
-    loadPage(window.location.pathname)
-    window.addEventListener("popstate", () => {
-      loadPage(window.location.pathname)
-    })
-  })
-}
-
-// =================== Обработчики =================
-document.addEventListener("DOMContentLoaded", () => {
-  startTimer()
-  doActiveLink()
-  showHideRightMenu()
-  swup()
+})
+Fancybox.bind("[data-fancybox]", {
+  // Your custom options
 })
